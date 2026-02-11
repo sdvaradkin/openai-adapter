6a780b1f120e8a9240a49965b5cc304b714641f3
a.semenas <a.semenas@godeltech.com>
2026-02-11 16:01:12 +0200

    fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: ['**/node_modules/**', '**/dist/**', 'tests/smoke.test.ts', 'tests/regression.test.ts'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/tests/integration/**'],
     environment: 'node',
     coverage: {
       reporter: ['text', 'json', 'html'],
